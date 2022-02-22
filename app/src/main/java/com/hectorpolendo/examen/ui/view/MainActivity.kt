dc9d253e209534dd027507fb377a149e10608449
HectorPolendo <62401577+HectorPolendo@users.noreply.github.com>
2022-02-21 23:34:26 -0600

    Initial commit

---
+++ app/src/main/java/com/hectorpolendo/examen/ui/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.hectorpolendo.examen.ui.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.hectorpolendo.examen.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
